ba133e3cc76a0634e7f7dd5e4c6d50fc514f5093
Febry <146858262+alfatfebry@users.noreply.github.com>
2025-09-13 16:13:52 +0700

    appp

--- app/page.js
+++ app/page.js
@@ -7,6 +7,12 @@ export default function Home() {
     <>
       {/* <AddSchool /> */}
       <ShowSchools />
+      <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+        <div className="bg-white rounded-lg shadow-lg w-11/12 md:w-2/3 lg:w-2xl p-6 relative">
+          <h2 className="text-xl font-bold">HARDCODE MODAL</h2>
+          <p>Ini harus nongol di Vercel kalau render jalan.</p>
+        </div>
+      </div>
     </>
   );
 }
